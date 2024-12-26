8e3862de7cc40746ae99a1b29017a597e816e3d1
paviraj1 <dhandumesendume@gmail.com>
2024-12-26 13:28:43 +0530

    new command

--- script2.js
+++ script2.js
@@ -1,1 +1,37 @@
-console.log("hello");
+// console.log("hello");
+class parent{
+    car()
+    {
+        console.log("BMW")
+    }
+    bike()
+    {console.log("royal enfild")} 
+
+}
+class child1 extends parent{
+    money()
+    {
+        console.log("10000$")
+    }
+}
+
+
+class child2 extends parent {
+    jewels() 
+    {
+        console.log("1KG")
+    }
+
+    house()
+    {
+        console.log("House document")
+    }
+}
+
+var childs1 = new child1()
+var childs2 = new child2()
+childs1.money()
+childs1.bike()
+childs2.jewels()
+childs2.house()
+childs2.car()
